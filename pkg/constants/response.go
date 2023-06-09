50925625ccbb6b06c4a436d676d33698dd1c11a2
HEEPOKE <Damon1fx@gmail.com>
2023-06-09 15:16:03 +0700

    :money_with_wings: update constant

--- pkg/constants/response.go
+++ pkg/constants/response.go
@@ -1,1 +1,44 @@
 package constants
+
+type ResponseMessage struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+const (
+	MESSAGE_SUCCESS              = "Success"
+	MESSAGE_FAIL                 = "Fail"
+	MESSAGE_INVALID              = "Invalid"
+	MESSAGE_NOTFOUND             = "Not Found"
+	MESSAGE_DUPLICATE            = "Duplicate"
+	STATUS_OK                    = 200
+	STATUS_CREATED               = 201
+	STATUS_BAD_REQUEST           = 400
+	STATUS_UNAUTHORIZED          = 401
+	STATUS_FORBIDDEN             = 403
+	STATUS_NOT_FOUND             = 404
+	STATUS_INTERNAL_SERVER_ERROR = 500
+)
+
+var (
+	SUCCESS = ResponseMessage{
+		Code:    "0000",
+		Message: MESSAGE_SUCCESS,
+	}
+	FAILED = ResponseMessage{
+		Code:    "0001",
+		Message: MESSAGE_FAIL,
+	}
+	NOT_FOUND = ResponseMessage{
+		Code:    "0002",
+		Message: MESSAGE_NOTFOUND,
+	}
+	DUPLICATE = ResponseMessage{
+		Code:    "0003",
+		Message: MESSAGE_DUPLICATE,
+	}
+	INVALID = ResponseMessage{
+		Code:    "0004",
+		Message: MESSAGE_INVALID,
+	}
+)
