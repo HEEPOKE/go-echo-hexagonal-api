ce285202bf4f9a131e04d770f2f506fd7ac76f52
HEEPOKE <Damon1fx@gmail.com>
2023-06-09 12:16:18 +0700

    :money_with_wings: update user model

--- internal/domains/models/user.go
+++ internal/domains/models/user.go
@@ -3,7 +3,6 @@ package models
 import (
 	"time"
 
-	"github.com/HEEPOKE/go-gin-hexagonal-api/pkg/enums"
 	"gorm.io/gorm"
 )
 
@@ -13,7 +12,6 @@ type User struct {
 	Username  string         `gorm:"type:VARCHAR(255);unique" json:"username"`
 	Password  string         `gorm:"type:VARCHAR(255)" json:"password"`
 	Tel       string         `gorm:"type:VARCHAR(255)" json:"tel"`
-	Role      enums.Role     `gorm:"type:ENUM('admin', 'user');default:'user'" json:"role"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
