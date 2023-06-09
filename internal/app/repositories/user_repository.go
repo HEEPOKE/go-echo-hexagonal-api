5a3ab5ec2d97ef98f389c9b10346b3ee998b8205
HEEPOKE <Damon1fx@gmail.com>
2023-06-09 14:22:34 +0700

    :money_with_wings: add repositories

---
+++ internal/app/repositories/user_repository.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"github.com/HEEPOKE/go-gin-hexagonal-api/internal/domains/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) GetAll() ([]*models.User, error) {
+	var users []*models.User
+	err := r.db.Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (r *UserRepository) GetByID(id int) (*models.User, error) {
+	var user models.User
+	err := r.db.First(&user, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
+}
