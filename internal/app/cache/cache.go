ef08d49f076d88093e329f56a3f93119b93e54d6
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-07-20 00:30:53 +0700

    :money_with_wings: set cache redis

--- internal/app/cache/cache.go
+++ internal/app/cache/cache.go
@@ -1,1 +1,17 @@
 package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/HEEPOKE/go-echo-hexagonal-api/pkg/database"
+)
+
+func SetKey(key string, value interface{}, expiration time.Duration) error {
+	client := database.ConnectToRedis()
+	return client.Set(context.Background(), key, value, expiration).Err()
+}
+func GetKey(key string) (string, error) {
+	client := database.ConnectToRedis()
+	return client.Get(context.Background(), key).Result()
+}
