502547c2bef23bce6427a8628f867cdb9e3630cf
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-09-16 19:21:06 +0700

    :money_with_wings: update schedu job

--- internal/core/utils/schedu.go
+++ internal/core/utils/schedu.go
@@ -1,16 +1,29 @@
 package utils
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/HEEPOKE/go-echo-hexagonal-api/internal/app/hooks"
+	"github.com/HEEPOKE/go-echo-hexagonal-api/pkg/config"
+	"github.com/go-co-op/gocron"
 )
 
 func ScheduleTaskDiscord(scheduTime time.Time, content string) error {
-	_, err := gocron.Every(1).Day().At(scheduTime.Format("15:04")).Do(hooks.SendDiscordMessage(), content)
+	loc, err := time.LoadLocation("Asia/Bangkok")
 	if err != nil {
 		return err
 	}
+
+	s := gocron.NewScheduler(loc)
+	_, err = s.At(scheduTime.Format("15:04")).Do(func() {
+		hooks.SendDiscordMessage("HEEPOKE", content, config.Cfg.DISCORD_URL)
+	})
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Test:: %s\n", scheduTime)
+	s.StartAsync()
 
 	return nil
 }
